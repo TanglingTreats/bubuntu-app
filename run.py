c0fed0b2e8c0e4ccc8663e2855f64b3c6328afaf
Ubuntu <ubuntu@ip-172-31-23-211.ap-southeast-1.compute.internal>
2019-07-12 03:25:02 +0000

    Update run.py. Change debug to False

--- run.py
+++ run.py
@@ -27,4 +27,4 @@ def get_message():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
