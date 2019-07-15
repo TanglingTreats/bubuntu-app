21a3dd04389aedcc1eda7320fe704158db7699b6
Ubuntu <ubuntu@ip-172-31-23-211.ap-southeast-1.compute.internal>
2019-07-15 15:47:06 +0000

    update ip to 0.0.0.0

--- run.py
+++ run.py
@@ -134,6 +134,6 @@ def execute_query(user_id, msg):
     
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
             port=int(os.environ.get("PORT", "5000")),
             debug=False)
